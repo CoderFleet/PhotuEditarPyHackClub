5e8a38f8ace4a01d1eae25356fee378badb45ea7
Rudransh Pratap Singh <87255169+CoderFleet@users.noreply.github.com>
2024-07-23 02:31:29 +0530

    more code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import io
 import argparse
 from PIL import Image, ImageOps, ImageEnhance, ImageFilter, ImageDraw, ImageFont, ImageChops, ImageCms
 
@@ -90,10 +92,88 @@ def handle_different_formats(image, format):
     output.seek(0)
     return Image.open(output)
 
+def process_image(image, args):
+    if args.resize:
+        image = resize_image(image, args.resize[0], args.resize[1])
+    if args.rotate:
+        image = rotate_image(image, args.rotate)
+    if args.grayscale:
+        image = convert_to_grayscale(image)
+    if args.crop:
+        image = crop_image(image, args.crop[0], args.crop[1], args.crop[2], args.crop[3])
+    if args.flip:
+        image = flip_image(image, args.flip)
+    if args.brightness:
+        image = adjust_brightness(image, args.brightness)
+    if args.blur:
+        image = blur_image(image, args.blur)
+    if args.contrast:
+        image = adjust_contrast(image, args.contrast)
+    if args.sharpen:
+        image = sharpen_image(image)
+    if args.edge_enhance:
+        image = edge_enhance_image(image)
+    if args.color:
+        image = adjust_color(image, args.color)
+    if args.saturation:
+        image = adjust_saturation(image, args.saturation)
+    if args.text:
+        if args.text_position and args.text_size and args.text_color:
+            image = add_text(image, args.text, tuple(args.text_position), args.text_size, args.text_color)
+        else:
+            print("Error: To add text, you must specify --text_position, --text_size, and --text_color")
+    if args.watermark:
+        if args.watermark_position:
+            image = add_watermark(image, args.watermark, tuple(args.watermark_position))
+        else:
+            print("Error: To add a watermark, you must specify --watermark_position")
+    if args.equalize:
+        image = equalize_histogram(image)
+    if args.invert:
+        image = invert_colors(image)
+    if args.blend:
+        if args.blend_alpha:
+            blend_image = load_image(args.blend)
+            if blend_image:
+                image = blend_images(image, blend_image, args.blend_alpha)
+            else:
+                print("Error: Could not load blend image")
+        else:
+            print("Error: To blend images, you must specify --blend_alpha")
+    if args.color_transform:
+        if len(args.color_transform) == 12:
+            matrix = tuple(args.color_transform)
+            image = apply_color_transform(image, matrix)
+        else:
+            print("Error: Color transform matrix must have 12 elements")
+    if args.format:
+        image = handle_different_formats(image, args.format)
+    return image
+
+def process_directory(input_dir, output_dir, args):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    summary = []
+    for file_name in os.listdir(input_dir):
+        if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+            input_path = os.path.join(input_dir, file_name)
+            output_path = os.path.join(output_dir, file_name)
+            image = load_image(input_path)
+            if image:
+                processed_image = process_image(image, args)
+                save_image(processed_image, output_path)
+                summary.append(f"Processed: {file_name}")
+
+    if summary:
+        print("\nSummary Report:")
+        for entry in summary:
+            print(entry)
+
 def main():
     parser = argparse.ArgumentParser(description="Command Line Photo Editor")
-    parser.add_argument("input", type=str, help="Path to the input image")
-    parser.add_argument("output", type=str, help="Path to save the output image")
+    parser.add_argument("input", type=str, help="Path to the input image or directory")
+    parser.add_argument("output", type=str, help="Path to save the output image or directory")
     parser.add_argument("--resize", type=int, nargs=2, metavar=('width', 'height'), help="Resize the image to the specified width and height")
     parser.add_argument("--rotate", type=int, metavar='angle', help="Rotate the image by the specified angle")
     parser.add_argument("--grayscale", action='store_true', help="Convert the image to grayscale")
@@ -118,66 +198,16 @@ def main():
     parser.add_argument("--blend_alpha", type=float, metavar='alpha', help="Alpha value for blending images")
     parser.add_argument("--color_transform", type=float, nargs=12, metavar='matrix', help="Apply a color transformation matrix to the image")
     parser.add_argument("--format", type=str, choices=['JPEG', 'PNG', 'BMP', 'GIF'], help="Convert the image to the specified format")
+    parser.add_argument("--batch", action='store_true', help="Process all images in the input directory")
     args = parser.parse_args()
 
-    image = load_image(args.input)
-    if image:
-        if args.resize:
-            image = resize_image(image, args.resize[0], args.resize[1])
-        if args.rotate:
-            image = rotate_image(image, args.rotate)
-        if args.grayscale:
-            image = convert_to_grayscale(image)
-        if args.crop:
-            image = crop_image(image, args.crop[0], args.crop[1], args.crop[2], args.crop[3])
-        if args.flip:
-            image = flip_image(image, args.flip)
-        if args.brightness:
-            image = adjust_brightness(image, args.brightness)
-        if args.blur:
-            image = blur_image(image, args.blur)
-        if args.contrast:
-            image = adjust_contrast(image, args.contrast)
-        if args.sharpen:
-            image = sharpen_image(image)
-        if args.edge_enhance:
-            image = edge_enhance_image(image)
-        if args.color:
-            image = adjust_color(image, args.color)
-        if args.saturation:
-            image = adjust_saturation(image, args.saturation)
-        if args.text:
-            if args.text_position and args.text_size and args.text_color:
-                image = add_text(image, args.text, tuple(args.text_position), args.text_size, args.text_color)
-            else:
-                print("Error: To add text, you must specify --text_position, --text_size, and --text_color")
-        if args.watermark:
-            if args.watermark_position:
-                image = add_watermark(image, args.watermark, tuple(args.watermark_position))
-            else:
-                print("Error: To add a watermark, you must specify --watermark_position")
-        if args.equalize:
-            image = equalize_histogram(image)
-        if args.invert:
-            image = invert_colors(image)
-        if args.blend:
-            if args.blend_alpha:
-                blend_image = load_image(args.blend)
-                if blend_image:
-                    image = blend_images(image, blend_image, args.blend_alpha)
-                else:
-                    print("Error: Could not load blend image")
-            else:
-                print("Error: To blend images, you must specify --blend_alpha")
-        if args.color_transform:
-            if len(args.color_transform) == 12:
-                matrix = tuple(args.color_transform)
-                image = apply_color_transform(image, matrix)
-            else:
-                print("Error: Color transform matrix must have 12 elements")
-        if args.format:
-            image = handle_different_formats(image, args.format)
-        save_image(image, args.output)
+    if os.path.isdir(args.input) and args.batch:
+        process_directory(args.input, args.output, args)
+    else:
+        image = load_image(args.input)
+        if image:
+            processed_image = process_image(image, args)
+            save_image(processed_image, args.output)
 
 if __name__ == "__main__":
     main()
